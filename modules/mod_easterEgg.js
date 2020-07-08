1350ed66346e20912ce97873dd6c2097d75b3143
Rémi Scandella <remi.scandella@gmail.com>
2020-07-08 14:26:46 +0200

    Remove useless easter egg code

--- modules/mod_easterEgg.js
+++ modules/mod_easterEgg.js
@@ -1,16 +1,9 @@
 // Anime le logo de la team Intuition quand on clique dessus, sur la section a-propos
 
-let gifEnCours = 0;
-
 export function playEasterEgg()
 {
-  if (!gifEnCours)
-  {
-    gifEnCours = 1;
-    const video = document.getElementById('instinct');
-    video.play();
-    setTimeout(() => { gifEnCours = 0; }, 2560);
-  }
+  const video = document.getElementById('instinct');
+  video.play();
 }
 
 export function prepareEasterEgg()
