d10395dc414c524e859c2c94c65f7cd8171bdf63
Rémi Scandella <remi.scandella@gmail.com>
2020-07-13 20:31:11 +0200

    Add note about why to keep redundancy

--- class_Sprite.php
+++ class_Sprite.php
@@ -11,6 +11,8 @@ class Sprite
     $sprite = str_replace('.png', '', $fichier);
     $sprite = explode('_', $sprite);
 
+    // Ne pas récupérer dexid ici, cela grossira les données stockées inutilement
+    // (chaque Pokémon stockerait son dexid dans chaque objet "Forme")
     $this->form = intval($sprite[3]);
     $this->gender = $sprite[4]; // uk = asexué, mf = indifférent mâle/femelle, md = mâle, fd = femelle, mo = mâle only, fo = femelle only
     $this->gigamax = ($sprite[5] == 'g') ? true : false;
