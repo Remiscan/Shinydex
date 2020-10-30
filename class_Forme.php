224d91f1ada9c141f465f917f38fd0b1b4f5bb64
Rémi Scandella <remi.scandella@gmail.com>
2020-10-30 03:54:23 +0100

    Ignore all identical Alcremie gigantamax formes

--- class_Forme.php
+++ class_Forme.php
@@ -30,6 +30,7 @@ class Forme extends Sprite
       || ($dexid == 777 && $sprite->form == 1) // Togedemaru totem
       || ($dexid == 784 && $sprite->form == 1) // Ékaïser totem
       || ($dexid == 849 && $sprite->form == 1 && $sprite->gigamax == 1) // Salarsen Gigamax Grave (identique au Aigu)
+      || ($dexid == 869 && $sprite->gigamax == 1 && ($sprite->form > 0 || $sprite->candy > 0)) // Charmilly Gigamax (autres friandises)
     )
       throw new Exception('Forme ignorée');
     
