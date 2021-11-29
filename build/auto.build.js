17af6f387a4bfa3f019e5226d603b552a2c5ac1c
Rémi Scandella <remi.scandella@gmail.com>
2021-11-29 01:13:11 +0100

    Copy non-TS files

--- build/auto.build.js
+++ build/auto.build.js
@@ -1,8 +1,17 @@
 import { compile } from './mod.js';
 
+
 const watcher = Deno.watchFs('./src');
 for await (const event of watcher) {
   console.log(`File system event:`, event);
-  try { compile(); }
-  catch (error) { console.log(error); continue; }
+  
+  for (const path of event.paths) {
+    if (path.endsWith('.ts')) {
+      try { compile(); }
+      catch (error) { console.log(error); continue; }
+    } else {
+      const newPath = path.replace('./src', './modules');
+      await Deno.copyFile(path, newPath);
+    }
+  }
 }
