9ef001dfb93d268c029c561391c947c30420796b
Rémi Scandella <remi.scandella@gmail.com>
2022-01-07 17:05:55 +0100

    Proprely remove files during build

--- build/auto.build.js
+++ build/auto.build.js
@@ -6,13 +6,14 @@ for await (const event of watcher) {
   console.log(`File system event:`, event);
   
   for (const path of event.paths) {
-    if (path.endsWith('.ts')) {
-      try { compile(); }
-      catch (error) { console.log(error); continue; }
-    } else {
-      const newPath = path.replace('./src', './modules');
-      try { await Deno.copyFile(path, newPath); }
-      catch (error) { console.log(error); continue; }
+    try {
+      const newPath = path.replace('./src', './modules')
+                          .replace('.ts', '.js');
+      if (event.kind === 'remove') await Deno.remove(newPath);
+      if (path.endsWith('.ts'))         compile(); // Compile whether a file got created, modified or removed
+      else if (event.kind !== 'remove') await Deno.copyFile(path, newPath);
+    } catch (error) {
+      console.log(error); continue;
     }
   }
 }
