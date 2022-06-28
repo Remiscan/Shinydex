50066f7b42681acf1aaa2cc997a9690aa95f4415
Rémi Scandella <remi.scandella@gmail.com>
2022-06-28 04:29:43 +0200

    Path should now work locally too

--- backend/parametres.php
+++ backend/parametres.php
@@ -2,8 +2,7 @@
 class Params
 {
   public static function path() {
-    $paramsPath = '../../_params/remidex.ini';
-    //$paramsPath = '/home/remiscanmk/params/remidex.ini';
+    $paramsPath = '/home/remiscanmk/params/remidex.ini';
     
     return $paramsPath;
   }
