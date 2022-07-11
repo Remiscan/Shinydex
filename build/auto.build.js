af4c43ecc6f64adba77f30a6944abc2da9fe7cd0
Rémi Scandella <remi.scandella@gmail.com>
2022-07-11 22:40:25 +0200

    Add comments

--- build/auto.build.js
+++ build/auto.build.js
@@ -7,9 +7,17 @@ for await (const event of watcher) {
   
   for (const path of event.paths) {
     try {
-      const newPath = path.replace('./src', './modules')
-                          .replace('.ts', '.js');
+      const compiledPath = path.replace('./src', './modules')
+                               .replace('.ts', '.js');
+
+      // If a file was deleted in ./src, delete the compiled or copied version in ./modules too
+      if (event.kind === 'remove') await Deno.remove(compiledPath);
+
+      // If a .ts file was created, modified or deleted, compile all modules
       if (path.endsWith('.ts'))         await compile();
+
+      // If a non-.ts file was created or modified, copy it into ./modules
+      else if (event.kind !== 'remove') await Deno.copyFile(path, compiledPath);
     } catch (error) {
       console.log(error); continue;
     }
