1652a660d4796fff5198b2c9ecb69ff4ad3513a4
Rémi Scandella <remi.scandella@gmail.com>
2022-12-25 23:26:12 +0100

    Adjust pokemon-card layout

--- src/components/pokemon-card/template.ts
+++ src/components/pokemon-card/template.ts
@@ -11,36 +11,32 @@ template.innerHTML = /*html*/`
     <svg><circle r="25" cx="25" cy="25"/></svg>
   </div>
 
-  <div class="pokemon-infos">
-    <div class="pokemon-infos__nom">
-      <span data-type="ball" class="icon"></span>
-      <span data-type="surnom"></span>
-      <span data-type="espece"></span>
-      <span data-type="gene"></span>
-    </div>
-
-    <div class="pokemon-infos__capture">
-      <span data-type="jeu" class="icon"></span>
-      <span data-type="methode"></span>
-      <span data-type="timeCapture"></span>
-      <span data-type="compteur"></span>
-    </div>
+  <div class="pokemon-infos__nom">
+    <span data-type="ball" class="icon"></span>
+    <span data-type="surnom"></span>
+    <span data-type="espece"></span>
+    <span data-type="gene"></span>
   </div>
 
-  <div class="pokemon-icones">
+  <div class="pokemon-infos__misc">
     <span data-type="checkmark" class="icon"></span>
+  </div>
 
-    <span class="spacer"></span>
-    
-    <div class="shiny-rate">
-      <div class="shiny-charm">
-        <span class="icon" data-icon="key/shiny-charm"></span>
-      </div>
-      <div class="shiny-rate-data">
-        <span class="shiny-rate-text numerator">1</span>
-        <span class="shiny-rate-text separator">/</span>
-        <span class="shiny-rate-text denominator">4096</span>
-      </div>
+  <div class="pokemon-infos__capture">
+    <span data-type="jeu" class="icon"></span>
+    <span data-type="methode"></span>
+    <span data-type="timeCapture"></span>
+    <span data-type="compteur"></span>
+  </div>
+
+  <div class="shiny-rate">
+    <div class="shiny-charm">
+      <span class="icon" data-icon="key/shiny-charm"></span>
+    </div>
+    <div class="shiny-rate-data">
+      <span class="shiny-rate-text numerator">1</span>
+      <span class="shiny-rate-text separator">/</span>
+      <span class="shiny-rate-text denominator">4096</span>
     </div>
   </div>
 
