4eccd56159eed14b7ab8e24cd5ca951c59b3b641
Rémi Scandella <remi.scandella@gmail.com>
2023-01-10 20:39:50 +0100

    Save sprites cache progress and display on launch

--- src/Settings.ts
+++ src/Settings.ts
@@ -64,8 +64,15 @@ export class Settings {
       setTheme(this['theme']);
 
     // Cache all sprites
-    if (!appliedSettings) {                                 // On app launch,
-      //if (this['cache-all-sprites']) cacheAllSprites(true); // Cache all new sprites.
+    if (!appliedSettings) { // On app launch
+      if (this['cache-all-sprites']) {
+        const progressContainer = document.querySelector('[data-sprites-progress]');
+        dataStorage.getItem('sprites-cache-progress')
+        .then(val => {
+          if (progressContainer && val) progressContainer.innerHTML = val;
+        });
+        // cacheAllSprites(true); // Cache all new sprites
+      }
     } else if (appliedSettings && this['cache-all-sprites'] !== appliedSettings['cache-all-sprites']) { // On manual settings change,
       cacheAllSprites(this['cache-all-sprites']);                                                       // cache or delete all sprites.
     }
@@ -140,17 +147,19 @@ export async function cacheAllSprites(bool: boolean): Promise<boolean> {
 
     if (progressContainer) progressContainer.innerHTML = 'Préparation...';
 
-    return await new Promise((resolve, reject) => {
+    let progress = 0, totalSize = 0, size = 0, unit = 'octets', unitPower = 0;
+    await new Promise((resolve, reject) => {
       const channel = new MessageChannel();
 
       channel.port1.onmessage = event => {
         if (progressContainer && event.data?.action === 'cache-all-sprites' && 'progress' in event.data) {
-          const progress = Math.floor(100 * (Number(event.data.progress) || 0));
+          progress = Math.floor(100 * (Number(event.data.progress) || 0));
           const progressWithErrors = Math.floor(100 * ((Number(event.data.progressWithErrors) || 0)));
-          const size = Math.round(event.data.totalSize * event.data.progress);
-          const unitPower = size > 10**6 ? 6 : size > 10**3 ? 3 : 0;
-          const unit = unitPower === 6 ? 'Mo' : unitPower === 3 ? 'ko' : 'octets';
-          const displayedSize = Math.round(100 * size / (10 ** unitPower)) / 100;
+          if (!totalSize) totalSize = event.data.totalSize;
+          size = Math.round(event.data.totalSize * event.data.progress);
+          unitPower = size > 10**6 ? 6 : size > 10**3 ? 3 : 0;
+          unit = unitPower === 6 ? 'Mo' : unitPower === 3 ? 'ko' : 'octets';
+          const displayedSize = (size / (10 ** unitPower)).toFixed(2);
 
           progressContainer.innerHTML = `${progress}% : ${displayedSize} ${unit}`;
 
@@ -165,6 +174,12 @@ export async function cacheAllSprites(bool: boolean): Promise<boolean> {
       
       worker.postMessage({ 'action': `cache-all-sprites` }, [channel.port2]);
     });
+
+    await dataStorage.setItem('sprites-cache-progress', `${progress}% : ${(size / (10 ** unitPower)).toFixed(2)} ${unit}`);
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
   } finally {
     if (input && 'disabled' in input) input.disabled = false;
   }
