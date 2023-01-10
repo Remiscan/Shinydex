cacbc64a708cf8f6df018f10bf6e30302b4a650a
Rémi Scandella <remi.scandella@gmail.com>
2023-01-11 00:41:04 +0100

    Highlight caught icons better

--- styles/themes.css.php
+++ styles/themes.css.php
@@ -3,6 +3,7 @@
   color-scheme: light;
   --bg-color: rgb(224, 224, 224);
   --accent-color: rgb(64, 81, 177);
+  --soft-accent-color : #c0caea;
   --nav-bg-color: rgb(235, 235, 235);
   --nav-text-color: rgb(100, 100, 100);
   --nav-bubble-color: rgb(179, 186, 224);
@@ -23,18 +24,20 @@
   --radio-checked-bg-color: white;
   --radio-check-color: var(--accent-color);
   --switch-unchecked-bg-color: hsl(231, 0%, 50%);
-  --button-hover-color: rgb(64, 81, 177, .2);
-  --button-active-color: rgb(64, 81, 177, .1);
+  --button-hover-color: rgb(64, 81, 177, .15);
+  --button-active-color: rgb(64, 81, 177, .2);
   --input-bg-color: white;
   --progress-bar-color: var(--accent-color);
   --card-edit-bg-color: rgba(240, 240, 240, .7);
   --dark-coeff: 0;
+  --pkmnicon-opacity: .3;
 }
 
 :root[data-theme="dark"] {
   color-scheme: dark;
   --bg-color: rgb(34, 34, 34);
   --accent-color: hsl(217, 89%, 75%);
+  --soft-accent-color: #212f45;
   --nav-bg-color: rgb(48, 48, 48);
   --nav-text-color: rgb(162, 166, 173);
   --nav-bubble-color: rgb(65, 74, 88);
@@ -55,12 +58,13 @@
   --radio-checked-bg-color: var(--accent-color);
   --radio-check-color: var(--nav-bg-color);
   --switch-unchecked-bg-color: hsla(217, 0%, 55%);
-  --button-hover-color: hsl(217, 89%, 75%, .2);
-  --button-active-color: hsl(217, 89%, 75%, .1);
+  --button-hover-color: hsl(217, 89%, 75%, .15);
+  --button-active-color: hsl(217, 89%, 75%, .2);
   --input-bg-color: hsl(0, 0%, 90%);
   --progress-bar-color: white;
   --card-edit-bg-color: hsla(0, 0%, 7%, .7);
   --dark-coeff: 1;
+  --pkmnicon-opacity: .5;
 }
 /*<?php themeSheetEnd(closeComment: true); ?>*/
 
