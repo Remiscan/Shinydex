3cd83b236bcd40d5300af48e56eb58e2c2244ec4
Rémi Scandella <remi.scandella@gmail.com>
2023-01-24 23:42:15 +0100

    Make input-select event more accessible

--- src/modules/components/inputSelect.ts
+++ src/modules/components/inputSelect.ts
@@ -1,3 +1,10 @@
+/**
+ * Partially derived from https://www.w3.org/WAI/ARIA/apg/example-index/combobox/combobox-select-only.html
+ * under W3C Software License (https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document)
+ */
+
+
+
 import { backFromTopLayer, closeTopLayer, openTopLayer, toTopLayer } from '../topLayer.js';
 import { TextField } from './textField.js';
 
@@ -13,14 +20,14 @@ template.innerHTML = /*html*/`
       <span class="label body-medium">
         <slot name="label"></slot>
       </span>
-      <button
-        type="button"
+      <span
+        tabindex="0"
         part="button"
         role="combobox"
         aria-haspopup="listbox"
         aria-expanded="false"
         aria-controls="options-list"
-      ></button>
+      ></span>
       <span class="material-icons trailing-icon" aria-hidden="true">
         <slot name="trailing-icon"></slot>
         <span class="error-icon">error</span>
@@ -53,8 +60,8 @@ template.innerHTML = /*html*/`
 
 const optionTemplate = document.createElement('template');
 optionTemplate.innerHTML = /*html*/`
-  <button type="button" role="option" id="option-{key}" class="surface interactive"
-    data-value="{value}" aria-selected="false" tabindex="-1"
+  <span role="option" id="option-{key}" class="surface interactive"
+    data-value="{value}" aria-selected="false"
     {attr}
   >
     <span class="material-icons" part="icon icon-unchecked" aria-hidden="true">
@@ -64,14 +71,14 @@ optionTemplate.innerHTML = /*html*/`
       <slot name="icon-checked">check</slot>
     </span>
     <span part="option-label" class="label-large">{label}</span>
-  </button type="button">
+  </span>
 `;
 
 
 
 const sheet = new CSSStyleSheet();
 sheet.replaceSync(/*css*/`
-  button {
+  [role="combobox"] {
     all: unset;
     grid-row: 2;
     grid-column: text;
@@ -81,7 +88,7 @@ sheet.replaceSync(/*css*/`
     color: rgb(var(--on-surface));
   }
 
-  button::before {
+  [role="combobox"]::before {
     content: '';
     width: 100%;
     height: 100%;
@@ -90,7 +97,7 @@ sheet.replaceSync(/*css*/`
     left: 0;
   }
 
-  button::after {
+  [role="combobox"]::after {
     content: 'expand_more';
     font-family: 'Material Icons';
     font-size: 24px;
@@ -103,6 +110,109 @@ sheet.replaceSync(/*css*/`
 `);
 
 
+/**
+ * 🔽 ARIA APG Select-Only Combobox code 🔽
+ */
+
+const actions = Object.fromEntries(Object.entries(
+  ['close', 'closeSelect', 'first', 'last', 'next', 'open', 'pageDown', 'pageUp', 'previous', 'select', 'type']
+).map(e => e.reverse()));
+
+function getActionFromKey(event: Event, menuOpen: boolean) {
+  if (!(event instanceof KeyboardEvent)) return;
+
+  const { key, altKey, ctrlKey, metaKey } = event;
+  const openKeys = ['ArrowDown', 'ArrowUp', 'Enter', ' '];
+
+  if (!menuOpen && openKeys.includes(key)) return actions.open;
+  if (key === 'Home') return actions.first;
+  if (key === 'End') return actions.last;
+
+  if (key === 'Backspace' || key === 'Clear' || (
+    key.length === 1 && key !== ' ' && !altKey && !ctrlKey && !metaKey
+  )) return actions.type;
+
+  if (menuOpen) {
+    if (key === 'ArrowUp' && altKey) return actions.closeSelect;
+    else if (key === 'ArrowDown' && !altKey) return actions.next;
+    else if (key === 'ArrowUp') return actions.previous;
+    else if (key === 'PageUp') return actions.pageUp;
+    else if (key === 'PageDown') return actions.pageDown;
+    else if (key === 'Escape') return actions.close;
+    else if (key === 'Enter' || key === ' ') return actions.closeSelect;
+  }
+}
+
+function filterOptions(options: string[] = [], filter: string, exclude: string[] = []) {
+  return options.filter(option => {
+    const matches = option.toLowerCase().indexOf(filter.toLowerCase()) === 0;
+    return matches && exclude.indexOf(option) < 0;
+  });
+}
+
+function getIndexByLetter(options: string[], filter: string, startIndex = 0) {
+  const orderedOptions = [
+    ...options.slice(startIndex),
+    ...options.slice(0, startIndex)
+  ];
+  const firstMatch = filterOptions(orderedOptions, filter)[0];
+  const allSameLetter = (array: any[]) => array.every(letter => letter === array[0]);
+
+  if (firstMatch) return options.indexOf(firstMatch);
+  else if (allSameLetter(filter.split(''))) {
+    const matches = filterOptions(orderedOptions, filter[0]);
+    return options.indexOf(matches[0]);
+  }
+  else return -1;
+}
+
+function getUpdatedIndex(currentIndex: number, maxIndex: number, action: string) {
+  const pageSize = 10;
+  switch (action) {
+    case actions.first: return 0;
+    case actions.last: return maxIndex;
+    case actions.previous: return Math.max(0, currentIndex - 1);
+    case actions.next: return Math.min(maxIndex, currentIndex + 1);
+    case actions.pageUp: return Math.max(0, currentIndex - pageSize);
+    case actions.pageDown: return Math.min(maxIndex, currentIndex + pageSize);
+    default: return currentIndex;
+  }
+}
+
+function isElementInView(element: Element) {
+  var bounding = element.getBoundingClientRect();
+
+  return (
+    bounding.top >= 0
+    && bounding.left >= 0
+    && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    && bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function isScrollable(element: HTMLElement) {
+  return element && element.clientHeight < element.scrollHeight;
+}
+
+function maintainScrollVisibility(activeElement: HTMLElement, scrollParent: HTMLElement) {
+  const { offsetHeight, offsetTop } = activeElement;
+  const { offsetHeight: parentOffsetHeight, scrollTop } = scrollParent;
+
+  const isAbove = offsetTop < scrollTop;
+  const isBelow = offsetTop + offsetHeight > scrollTop + parentOffsetHeight;
+
+  if (isAbove) {
+    scrollParent.scrollTo(0, offsetTop - 4);
+  } else if (isBelow) {
+    scrollParent.scrollTo(0, offsetTop - parentOffsetHeight + offsetHeight + 4);
+  }
+}
+
+/**
+ * 🔼 ARIA APG Select-Only Combobox code 🔼
+ */
+
+
 
 export class InputSelect extends TextField {
   static template = template;
@@ -111,6 +221,10 @@ export class InputSelect extends TextField {
   static defaultValue = 'null';
   #initialSlotsAssigned = false; // to check if options need to be generated from slot in connectedCallback
   #optionsList: HTMLElement | undefined; // to retain control over the options list when it's promoted to the top layer
+  #button: HTMLElement | undefined;
+  #ignoreBlur = false; // to keep focus on the button while using the select menu
+  #searchString = '';
+  #searchTimeout?: number;
 
   labels: Map<string, string> = new Map(); // stores options as couples of values and labels
 
@@ -176,91 +290,131 @@ export class InputSelect extends TextField {
   };
 
 
-  // Opens the select menu on pressing some keys while the button is focused.
-  buttonKeydownHandler = (event: Event) => {
-    const button = event.target;
-    if (!(button instanceof HTMLButtonElement)) return;
-    if (!(event instanceof KeyboardEvent)) return;
-
-    if (!this.isOpen && ['ArrowDown', 'ArrowUp'].includes(event.key)) {
-      return button.click();
-    }
-  }
-
-
-  // Enables keyboard navigation in the options list.
-  optionKeydownHandler = (event: Event) => {
-    if (!(event instanceof KeyboardEvent)) return;
-    const { key, altKey } = event;
-
-    switch (key) {
-      case 'ArrowUp':
-        if (altKey) this.close();
-        else        this.focusedIndex--;
-        break;
-
-      case 'ArrowDown':
-        if (altKey) return;
-        else        this.focusedIndex++;
-        break;
-
-      case 'PageUp':
-        this.focusedIndex -= 10;
-        break;
-
-      case 'PageDown':
-        this.focusedIndex += 10;
-        break;
-
-      case 'Home':
-        this.focusedIndex = 0;
-        break;
-
-      case 'End':
-        this.focusedIndex = +Infinity;
-        break;
-
-      case 'Escape':
-        this.close();
-        break;
-    }
-  };
-
-
   // Close the menu on clicking anywhere in the window, except on the menu itself.
-  optionBlurHandler = (event: Event) => {
+  buttonBlurHandler = (event: Event) => {
     if (!(event instanceof FocusEvent)) return;
+    if (!(event.target instanceof HTMLElement)) return;
 
-    const blurredElement = event.target;
-    const focusedElement = event.relatedTarget;
-    const topLayer = document.querySelector('#top-layer');
-    if (topLayer && focusedElement instanceof Node && 
-      (topLayer.contains(focusedElement) || this.shadow.contains(focusedElement))
-    ) return;
+    if (this.#ignoreBlur) {
+      this.#ignoreBlur = false;
+      return;
+    }
 
-    this.selectIndex(this.focusedIndex);
-    this.close();
+    if (this.isOpen) {
+      this.selectIndex(this.focusedIndex);
+      this.close(false);
+    }
   }
-  
+
 
   // Opens the options list in the top layer, and listens for its changes.
   buttonClickHandler = (event: Event) => {
-    if (!this.isOpen) this.open();
-    else              this.close();
+    if (!this.isOpen) this.open(false);
+    else {
+      //this.focusedIndex = this.valueIndex;
+      this.close(false);
+    }
   };
 
 
+  // Enables keyboard navigation in the options list.
+  buttonKeydownHandler = (event: Event) => {
+    const button = event.target;
+    if (!(button instanceof HTMLElement)) return;
+    if (!(event instanceof KeyboardEvent)) return;
+
+    const { key } = event;
+    const max = this.allOptions.length - 1;
+    const action = getActionFromKey(event, this.isOpen);
+
+    switch (action) {
+      case actions.last:
+      case actions.first:
+        this.open();
+        // don't break
+      case actions.next:
+      case actions.previous:
+      case actions.pageUp:
+      case actions.pageDown:
+        event.preventDefault();
+        return this.focusedIndex = getUpdatedIndex(this.focusedIndex, max, action);
+      case actions.closeSelect:
+        event.preventDefault();
+        this.selectIndex(this.focusedIndex);
+        // don't break
+      case actions.close:
+        event.preventDefault();
+        return this.close();
+      case actions.type:
+        return this.typeHandler(key);
+      case actions.open:
+        event.preventDefault();
+        return this.open();
+    }
+  };
+
+
+  // Opens the menu and selects the first option starting by the typed letter.
+  typeHandler = (letter: string) => {
+    this.open();
+
+    const searchString = this.getSearchString(letter);
+    const searchIndex = getIndexByLetter(
+      this.allOptions.map(option => this.getLabel(option.getAttribute('data-value') ?? 'null')),
+      searchString,
+      this.focusedIndex + 1
+    );
+
+    if (searchIndex >= 0) this.focusedIndex = searchIndex;
+    else {
+      clearTimeout(this.#searchTimeout);
+      this.#searchString = '';
+    }
+  }
+
+
+  // Prevents focus from leaving the button when clicking on an option.
+  optionPointerdownHandler = (event: Event) => {
+    this.#ignoreBlur = true;
+  }
+
+
   // Selects a value and closes the menu on a click on an option.
-  optionClickHandler = (event: Event) => {
-    const option = event.currentTarget;
+  optionsListClickHandler = (event: Event) => {
+    if (!(event.target instanceof HTMLElement) || !(event.currentTarget instanceof HTMLElement)) return;
+    const option = event.target?.closest('[role="option"]');
     if (!(option instanceof HTMLElement)) return;
+    if (!(event.currentTarget.contains(option))) return;
+
     this.selectValue(option.getAttribute('data-value') ?? this.defaultValue);
     this.close();
   }
 
 
+  // Builds the search string by adding the newly types character to the previously typed string (in the last 500ms)
+  getSearchString(char: string) {
+    if (typeof this.#searchTimeout === 'number') clearTimeout(this.#searchTimeout);
+    this.#searchTimeout = setTimeout(() => this.#searchString = '', 500);
+    this.#searchString += char;
+    return this.#searchString;
+  }
+
+
+  // Opens or closes the select menu
+  toggleMenu(open: boolean, restoreFocus = true) {
+    if (this.isOpen === open) return;
+
+    if (open) this.open();
+    else      this.close();
+
+    if (restoreFocus) this.button?.focus();
+  }
+
+
   // Opens the menu and promotes the options list to the top layer.
-  open() {
+  open(restoreFocus = true) {
+    if (this.isOpen) return;
+
     const optionsList = this.optionsList;
     if (!(optionsList instanceof HTMLElement)) throw new Error('Expecting HTMLElement');
 
@@ -316,32 +470,28 @@ export class InputSelect extends TextField {
 
     // Focus the selected option
     const selectedOption = optionsList.querySelector('[role="option"][aria-selected="true"]');
-    if (selectedOption instanceof HTMLButtonElement) {
+    if (selectedOption instanceof HTMLElement) {
       const selectedIndex = this.indexOf(selectedOption?.getAttribute('data-value'));
       this.focusedIndex = selectedIndex;
     }
 
     // Make the menu more accessible and be ready to close it.
-    optionsList.querySelectorAll('[role="option"]').forEach(option => {
-      option.addEventListener('click', this.optionClickHandler);
-      option.addEventListener('blur', this.optionBlurHandler);
-      option.addEventListener('keydown', this.optionKeydownHandler);
-    });
-    this.button?.addEventListener('blur', this.optionBlurHandler);
+    this.allOptions.forEach(option => option.addEventListener('pointerdown', this.optionPointerdownHandler));
+    optionsList.addEventListener('click', this.optionsListClickHandler);
+
+    if (restoreFocus) this.button?.focus();
   }
 
 
   // Closes the menu and brings it back from the top layer.
-  close() {
+  close(restoreFocus = true) {
+    if (!this.isOpen) return;
+
     const optionsList = this.optionsList;
     if (!(optionsList instanceof HTMLElement)) throw new Error('Expecting HTMLElement');
 
-    optionsList.querySelectorAll('[role="option"]').forEach(option => {
-      option.removeEventListener('click', this.optionClickHandler);
-      option.removeEventListener('blur', this.optionBlurHandler);
-      option.removeEventListener('keydown', this.optionKeydownHandler);
-    });
-    this.button?.removeEventListener('blur', this.optionBlurHandler);
+    optionsList.removeEventListener('click', this.optionsListClickHandler);
+    this.allOptions.forEach(option => option.removeEventListener('pointerdown', this.optionPointerdownHandler));
 
     closeTopLayer();
     optionsList.setAttribute('hidden', '');
@@ -349,6 +499,8 @@ export class InputSelect extends TextField {
 
     this.shadow.querySelector('label')?.classList.remove('focused');
     this.button?.setAttribute('aria-expanded', 'false');
+
+    if (restoreFocus) this.button?.focus();
   }
 
 
@@ -399,16 +551,20 @@ export class InputSelect extends TextField {
     key = Math.max(0, Math.min(allOptions.length - 1, key));
     for (let k = 0; k < allOptions.length; k++) {
       const option = allOptions[k];
-      if (!(option instanceof HTMLButtonElement)) continue;
+      if (!(option instanceof HTMLElement)) continue;
       if (k === key) {
-        option.tabIndex = 0;
-        option.focus();
+        /*option.tabIndex = 0;
+        option.focus();*/
         option.classList.add('focused');
         const button = this.button;
         button?.setAttribute('aria-activedescendant', `option-${k}`);
+
+        // Make sure the option is on screen
+        if (this.optionsList && isScrollable(this.optionsList)) maintainScrollVisibility(option, this.optionsList);
+        if (!isElementInView(option)) option.scrollIntoView( { behavior: 'smooth', block: 'nearest' });
       }
       else {
-        option.tabIndex = -1;
+        //option.tabIndex = -1;
         option.classList.remove('focused');
       }
     }
@@ -419,13 +575,6 @@ export class InputSelect extends TextField {
     return this.allOptions[this.focusedIndex].getAttribute('data-value');
   }
 
-
-  // Container of all option elements.
-  get optionsList(): HTMLElement | undefined {
-    if (this.#optionsList) return this.#optionsList;
-    const optionsList = this.shadow.querySelector('[role="listbox"]');
-    if (optionsList instanceof HTMLElement) return optionsList;
-  }
 
   // Array of all option elements.
   get allOptions(): Element[] {
@@ -445,10 +594,19 @@ export class InputSelect extends TextField {
   }
 
 
+  // Container of all option elements.
+  get optionsList(): HTMLElement | undefined {
+    if (this.#optionsList) return this.#optionsList;
+    const optionsList = this.shadow.querySelector('[role="listbox"]');
+    if (optionsList instanceof HTMLElement) return optionsList;
+  }
+
+
   // Button that opens the menu.
-  get button(): HTMLButtonElement | undefined {
-    const button = this.shadow.querySelector('button');
-    if (button instanceof HTMLButtonElement) return button;
+  get button(): HTMLElement | undefined {
+    if (this.#button) return this.#button;
+    const button = this.shadow.querySelector('[role="combobox"]');
+    if (button instanceof HTMLElement) return button;
   }
 
 
@@ -504,6 +662,13 @@ export class InputSelect extends TextField {
     const optionsList = this.optionsList;
     if (optionsList instanceof HTMLElement) this.#optionsList = optionsList;
 
+    const button = this.button;
+    if (button instanceof HTMLElement) this.#button = button;
+
+    button?.addEventListener('blur', this.buttonBlurHandler);
+    button?.addEventListener('click', this.buttonClickHandler);
+    button?.addEventListener('keydown', this.buttonKeydownHandler);
+
     // To rebuild the list of options when the slot's contents change.
     const source = this.shadow.querySelector('datalist > slot');
     source?.addEventListener('slotchange', this.slotchangeHandler);
@@ -517,19 +682,18 @@ export class InputSelect extends TextField {
       }
       this.#initialSlotsAssigned = true;
     }
-
-    const button = this.button;
-    button?.addEventListener('click', this.buttonClickHandler);
-    button?.addEventListener('keydown', this.buttonKeydownHandler);
   }
 
   disconnectedCallback(): void {
-    const source = this.shadow.querySelector('datalist > slot');
-    source?.removeEventListener('slotchange', this.slotchangeHandler);
+    if (this.isOpen) this.close(false);
 
     const button = this.button;
+    button?.removeEventListener('blur', this.buttonBlurHandler);
     button?.removeEventListener('click', this.buttonClickHandler);
     button?.removeEventListener('keydown', this.buttonKeydownHandler);
+
+    const source = this.shadow.querySelector('datalist > slot');
+    source?.removeEventListener('slotchange', this.slotchangeHandler);
   }
 }
 
