1e9118c7cc12475502ca80749de045762f0d5fb2
Rémi Scandella <remi.scandella@gmail.com>
2023-03-05 00:07:42 +0100

    Fix mobile keyboard behaviour

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta name="viewport" content="width=device-width, user-scalable=no, interactive-widget=resizes-content">
     <meta name="theme-color" content="#222222">
 
     <title>Shinydex</title>
