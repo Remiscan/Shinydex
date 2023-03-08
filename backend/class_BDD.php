f3c1bc1e336cebdd12f6d0e57e98e33d0ea41431
Rémi Scandella <remi.scandella@gmail.com>
2023-03-08 17:21:37 +0100

    Access DB password the right way

--- backend/class_BDD.php
+++ backend/class_BDD.php
@@ -4,7 +4,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/_common/php/DotEnv.php';
 class BDD extends PDO {
   public function __construct() {
     $params = (new DotEnv('/home/remiscan/remiscanfr/config/database/mysql.env'))->getAll();
-    $params['password'] = file_get_contents('/home/remiscan/remiscanfr/secrets/database/user_password.txt');
+    $params['password'] = rtrim(file_get_contents('/run/secrets/database_user_password'));
 
     parent::__construct("mysql:host=mysql;dbname=".$params['MYSQL_DATABASE'], $params['MYSQL_USER'], $params['password']);
   }
