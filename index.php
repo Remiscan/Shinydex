9e481a86fb117414087638f3049d9b37aa4682c8
Rémi Scandella <remi.scandella@gmail.com>
2025-05-07 18:09:29 +0000

    Revert edge-to-edge support for now

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $httpLanguage = $translation->getLanguage();
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content, viewport-fit=cover">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content">
     <meta name="theme-color" content="#222222">
     <meta name="theme-color" id="medium-layout-theme-color" media="screen and (min-width: 720px)" content="#222222" data-forced-color="surface-container">
 
